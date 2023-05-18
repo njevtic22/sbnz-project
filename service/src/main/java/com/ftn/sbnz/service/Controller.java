0a2d3f980c2b96539cbf91386c37527ef5488146
Nemanja Jevtić <njevtic22@gmail.com>
2023-05-18 20:49:56 +0200

    Initial applications.

---
+++ service/src/main/java/com/ftn/sbnz/service/Controller.java
@@ -0,0 +1,31 @@
+package com.ftn.sbnz.service;
+
+import com.ftn.sbnz.model.model.Message;
+import com.ftn.sbnz.model.util.KnowledgeSessionHelper;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/messages")
+public class Controller {
+    @PostMapping
+    public ResponseEntity<Void> addMessage() {
+        KieContainer kc = KnowledgeSessionHelper.createRuleBase();
+        KieSession kSession = KnowledgeSessionHelper.getStatefulKnowledgeSession(kc, "k-session");
+
+
+        // KieSession kSession1 = kContainer.newKieSession("example-session");
+        // insertovanje fact-a
+        Message message = new Message();
+        message.setMessage("Hello World");
+        message.setStatus(Message.HELLO);
+        kSession.insert(message);
+        kSession.fireAllRules();
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
@@ -0,0 +1,44 @@
+package com.ftn.sbnz.service;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ServiceApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+   
+    @Bean
+	public KieContainer kieContainer()
+		{
+			KieServices ks = KieServices.Factory.get();
+			KieContainer kContainer = ks
+					.newKieContainer(ks.newReleaseId("com.ftn.sbnz", "kjar", "0.0.1-SNAPSHOT"));
+			KieScanner kScanner = ks.newKieScanner(kContainer);
+			kScanner.start(1000);
+			return kContainer;
+		}
+		
+	@Override
+	public void run(String... args) throws Exception 
+	{
+	
+		// pokretati odvojeno svaku main klasu
+//		Test.main();
+		
+		//GlobalExample.main();
+		
+		//ModifyExample.main();
+		//AgendaGroupExample.main();
+		//DeleteExample.main();
+		
+	}
+
+}
