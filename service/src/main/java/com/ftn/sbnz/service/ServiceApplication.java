4c4cfe4ba57e9a8adeb6d91f210c74836ea44272
Nemanja Jevtić <njevtic22@gmail.com>
2023-05-20 18:03:33 +0200

    Implemented logging exceptions.

--- service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
+++ service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
@@ -3,7 +3,9 @@ package com.ftn.sbnz.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @EntityScan("com.ftn.sbnz.model")
 @SpringBootApplication
 public class ServiceApplication {
---
+++ service/src/main/java/com/ftn/sbnz/service/logging/LoggingAspect.java
@@ -0,0 +1,51 @@
+package com.ftn.sbnz.service.logging;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("within(com.ftn.sbnz.service.core.error.RestExceptionHandler) && " +
+            "!execution(* com.ftn.sbnz.service.core.error.RestExceptionHandler.handleInternalServer(..))")
+    private void exceptionHandlerPointcut() { }
+
+    @Async
+    @Before("exceptionHandlerPointcut()")
+    protected void logException(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        Exception ex = (Exception) args[0];
+        logger.warn(ex.getMessage(), ex);
+    }
+
+    // TODO: add when security implemented
+//    @Pointcut("execution(* com.ftn.sbnz.service.security.rest.RestAuthenticationEntryPoint.commence(..))")
+//    private void authenticationExceptionHandlerPointcut() { }
+//
+//    @Async
+//    @Before("authenticationExceptionHandlerPointcut()")
+//    protected void logAuthenticationException(JoinPoint joinPoint) {
+//        Object[] args = joinPoint.getArgs();
+//        Exception ex = (Exception) args[2];
+//        logger.warn(ex.getMessage(), ex);
+//    }
+
+    @Pointcut("execution(* com.ftn.sbnz.service.core.error.RestExceptionHandler.handleInternalServer(..))")
+    private void internalServerErrorHandlerPointcut() { }
+
+    @Async
+    @Before("internalServerErrorHandlerPointcut()")
+    protected void logInternalServerError(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        Exception ex = (Exception) args[0];
+        logger.error(ex.getMessage(), ex);
+    }
+}
