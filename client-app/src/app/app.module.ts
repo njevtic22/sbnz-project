38f5ac0a8011af243eedec0f6c93034c74931cbb
Nemanja Jevtić <njevtic22@gmail.com>
2023-05-28 13:10:59 +0200

    Initialized angular app.

---
+++ client-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
