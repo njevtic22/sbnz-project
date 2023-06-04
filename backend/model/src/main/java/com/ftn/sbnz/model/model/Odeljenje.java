1be70a3dd612ad493e792f90a9725f9a29bc04d4
Nemanja Jevtić <njevtic22@gmail.com>
2023-06-04 14:32:53 +0200

    Minor changes.

--- backend/model/src/main/java/com/ftn/sbnz/model/model/Odeljenje.java
+++ backend/model/src/main/java/com/ftn/sbnz/model/model/Odeljenje.java
@@ -5,8 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import java.util.List;
@@ -24,7 +24,7 @@ public class Odeljenje {
     @Column(nullable = false)
     private String naziv;
 
-    @OneToOne
+    @ManyToOne
     private Teacher staresina;
 
     @OneToMany
